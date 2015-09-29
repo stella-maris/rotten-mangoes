361cdc07f1d2687c3a44273f8708862adf67490f
Mary Lee <mango.gal@gmail.com>
2015-09-29 21:04:32 +0000

    generates review model

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,9 @@ RottenMangoes::Application.routes.draw do
 
   get 'users/create'
 
-  resources :movies
+  resources :movies do
+    resources :reviews, only: [:new, :create]
+  end
   resources :users, only: [:new, :create]
   resources :sessions, only: [:new, :create, :destroy]
 
